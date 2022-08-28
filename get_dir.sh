a1cc08c10804e0b72bfb69fbea2771ee165e3418
ajay253517 <ajay.baljoshi@gmail.com>
2022-08-28 12:43:32 +0530

    testing git changed dir

--- get_dir.sh
+++ get_dir.sh
@@ -1,5 +1,7 @@
 #!/bin/bash 
 echo "get the changed directory"
 git fetch --all
+git pull --all
+git log --oneline
 git diff --name-only HEAD@{0} HEAD@{1} | awk -F / '{print $1}'
 echo "--------------------------------------------"
