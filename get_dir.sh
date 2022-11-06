06780b2a47e053d4555d7af5b1021ce6fb350c1e
ajay253517 <ajay.baljoshi@gmail.com>
2022-11-06 19:49:00 +0530

    updated get directory

--- get_dir.sh
+++ get_dir.sh
@@ -1,12 +1,5 @@
 #!/bin/bash 
 echo "get the changed directory"
-git fetch --all
-echo "------------------------"
-git pull --all
-echo "------------------------"
-git log --oneline
 echo "--------------------------------"
-git diff --dirstat=files,0 HEAD~1 | sed 's/^[ 0-9.]\+% //g' | grep -v github
-echo "--------------------------"
-git diff --name-only HEAD@{0} HEAD@{1} | awk -F / '{print $1}'
+git diff --dirstat=files,0 HEAD~1 | sed 's/^[ 0-9.]\+% //g' | grep -v github > /tmp/chg_dir
 echo "--------------------------------------------"
